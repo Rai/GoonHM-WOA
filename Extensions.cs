0f21f0f4a2fcb51922aaeecd195f8e8bd9188d1b
Vinny M <rais.git@gmail.com>
2023-10-14 02:33:07 -0400

    formatting

--- Extensions.cs
+++ Extensions.cs
@@ -6,8 +6,8 @@ namespace GoonHW_WOA
     public static class Extensions
     {
             public static NpcInfo.NpcData[] GetNpcData(this NpcInfo npcInfo)
-    {
-        return (NpcInfo.NpcData[])AccessTools.Field(typeof(NpcInfo), "Data").GetValue(npcInfo);
-    }
+            {
+                return (NpcInfo.NpcData[])AccessTools.Field(typeof(NpcInfo), "Data").GetValue(npcInfo);
+            }
     }
 }
