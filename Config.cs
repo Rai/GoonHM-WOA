da7da8a6f0a4ea8b338223ff9df009b724cdb354
Vinny M <rais.git@gmail.com>
2023-10-14 02:25:15 -0400

    Add configuration

--- Config.cs
+++ Config.cs
@@ -5,8 +5,24 @@ namespace GoonHW_WOA;
 
 public partial class GoonHW_WOA
 {
+    public static ConfigEntry<bool> enableDebugMenu;
+    public static ConfigEntry<bool> skipIntro;
+    public static ConfigEntry<bool> showItemIdInInventory;
+    public static ConfigEntry<bool> showItemResourceInInventory;
+
+    public static ConfigEntry<bool> showFavorabilityInNpcNote;
+
+    public static ConfigEntry<bool> showGiftedAndTalkedInNpcNote;
+
     private void InitConfig()
     {
-        // TODO: Add config options..
+        enableDebugMenu = Config.Bind<bool>("General", "Enable Debug Menu", true, "Enable debug menu.");
+        skipIntro = Config.Bind<bool>("General", "Skip Intro", true, "Skip the intro.");
+
+        showFavorabilityInNpcNote = Config.Bind<bool>("NpcMenu", "Show Favorability", true, "Show favorability in NPC description.");
+        showGiftedAndTalkedInNpcNote = Config.Bind<bool>("NpcMenu", "Show Gifted and Talked", true, "Show gifted and talked status in NPC description.");
+        
+        showItemIdInInventory = Config.Bind<bool>("Inventory", "Show Item ID", true, "Show item ID in inventory. (after the item name)");
+        showItemResourceInInventory = Config.Bind<bool>("Inventory", "Show Item Resource ID", false, "Show an additional item resource ID in inventory. (after the Item ID)");
     }
 }
--- Patches/ConveniencePatches.cs
+++ Patches/ConveniencePatches.cs
@@ -11,24 +11,31 @@ public class ConveniencePatches
         [HarmonyPrefix]
         public static void SkipIntro(ref float ____timeCount)
         {
-            ____timeCount = 10f;
+            if(GoonHW_WOA.skipIntro.Value)
+                ____timeCount = 10f;
         }
 
         [HarmonyPatch(typeof(ItemBagItemDetail), "ViewItemDetail")]
         [HarmonyPostfix]
         public static void ItemBagItemDetail_ViewItemDetail(ref ItemBagItemDetail __instance, ref TextMeshProEx ____itemName, Table.ITEMCATEGORYGROUP.Defs categoryGroup, Table.ITEMPARAM.Defs itemId, Sprite iconSprite, string itemName)
         {
-            ____itemName.text = itemName + " - " + ((uint)itemId) + " - " + itemId.ToString();
+            if(GoonHW_WOA.showItemIdInInventory.Value)
+                ____itemName.text = itemName + " - " + ((uint)itemId) + (GoonHW_WOA.showItemResourceInInventory.Value ? " - " + itemId.ToString() : "");
         }
 
         [HarmonyPatch(typeof(NpcNoteDetail), "UpdateDetails")]
         [HarmonyPostfix]
         public static void NpcNoteDetail_UpdateDetails(int idx, ref TextMeshProEx ____npcDetail)
         {
+            if(!GoonHW_WOA.showFavorabilityInNpcNote.Value && !GoonHW_WOA.showGiftedAndTalkedInNpcNote.Value)
+                return;
+
             var fav = SingletonMonoBehaviour<GameInfo>.Instance.NpcInfo.GetFavorability(idx);
             var gifted = SingletonMonoBehaviour<GameInfo>.Instance.NpcInfo.GetGift(idx);
             var talked = SingletonMonoBehaviour<GameInfo>.Instance.NpcInfo.GetTalked(idx);
+            
             ____npcDetail.text = ____npcDetail.text + "\n"
-            + $"Favorability: {fav} Gifted: {(gifted ? "yes" : "no")} Talked: {(talked ? "yes" : "no")}";
+            + (GoonHW_WOA.showFavorabilityInNpcNote.Value ? $"Favorability: {fav}" : "") 
+            + (GoonHW_WOA.showGiftedAndTalkedInNpcNote.Value ? $" Gifted: {(gifted ? "yes" : "no")} Talked: {(talked ? "yes" : "no")}" : "");
         }
     }
